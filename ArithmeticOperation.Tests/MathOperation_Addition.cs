37a940531c305080c1b2a0e0c2cbb28c3bbdf759
Thierry Morre <thierry.morre@cognitic.be>
2025-08-22 12:06:29 +0200

    Ajoutez des fichiers projet.

---
+++ ArithmeticOperation.Tests/MathOperation_Addition.cs
@@ -0,0 +1,47 @@
+namespace ArithmeticOperation.Tests
+{
+    public class MathOperation_Addition
+    {
+        [Fact]
+        public void TestAddition_Simple1()
+        {
+            //Arrange (Préparation du Test)            
+            int x = 5, y = 3;
+
+            //Act (Appel de la fonctionnalité)
+            int result = MathOperation.Addition(x, y);
+
+            //Assert (Vérification des résultats)
+            Assert.Equal(8, result);
+        }
+
+        [Theory]
+        //Arrange (Préparation du Test)            
+        [InlineData(5, 3, 8)]
+        [InlineData(-5, 3, -2)]
+        [InlineData(5, -3, 2)]
+        [InlineData(4096, 24, 4120)]
+        public void TestAddition_Simple2(in int x, in int y, in int result)
+        {
+            //Act (Appel de la fonctionnalité)
+            int local_result = MathOperation.Addition(x, y);
+
+            //Assert (Vérification des résultats)
+            Assert.Equal(result, local_result);
+        }
+
+        [Theory]
+        //Arrange
+        [InlineData(int.MaxValue, 1)]
+        [InlineData(int.MinValue, -1)]
+        public void TestAddition_LimitOverflowUnderFlow(int x, int y)
+        {
+            //Act
+            Exception e = Record.Exception(() => MathOperation.Addition(x, y));
+
+            //Assert
+            Assert.IsType<OverflowException>(e);
+            Assert.Equal("Arithmetic operation resulted in an overflow.", e.Message);
+        }
+    }
+}
---
+++ ArithmeticOperation/MathOperation.cs
@@ -0,0 +1,8 @@
+﻿namespace ArithmeticOperation
+{
+    public static class MathOperation
+    {
+        public static int Addition(int a, int b) => checked(a + b);
+        public static int Division(int a, int b) => a / b;
+    }
+}
